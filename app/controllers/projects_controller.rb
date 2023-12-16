8606b114ab98a2f010d020ac820914e4cba1123c
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-12-16 19:35:47 +0000

    Add Pundit Authorizations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,21 +1,23 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show]
+  before_action :set_project, only: [:show, :edit, :update]
   # before_action :authenticate_user!
 
 
   def index
-    @projects = Project.all
+    @projects = policy_scope(Project)
   end
 
   def new
     @project = Project.new
+    authorize @project
   end
 
   def create
     @project = Project.new(project_params)
     @project.user = current_user
+    authorize @project
     if @project.save
-      redirect_to projects_path, notice: "Project added!"
+      redirect_to projects_path, notice: "Project added!", status: :see_other
     else
       render :new, status: :unprocessable_entity
     end
@@ -23,8 +25,21 @@ class ProjectsController < ApplicationController
 
   def show
     # @project = Project.find(params[:id])
+    authorize @project
   end
 
+  def edit
+    authorize @project
+  end
+
+  def update
+    authorize @project
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project was updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  validates_presence_of :name, :description, :github, :url
   belongs_to :user
 end
