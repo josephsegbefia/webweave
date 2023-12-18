01708f2efb14b08582f1d71da33f2b13170b07f3
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-12-18 16:57:44 +0000

    Dashboard done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboards/show'
   resources :projects, only: [:index, :new, :create, :show, :edit]
+  resources :dashboards, only: [:show]
   devise_for :users
   root to: "projects#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_16_201937) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_18_163616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_16_201937) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "dashboards", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dashboards_on_user_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -67,5 +74,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_16_201937) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "dashboards", "users"
   add_foreign_key "projects", "users"
 end
