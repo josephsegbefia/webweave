e2b002701c1a33c2213c4a66b7b9679b18a17d6b
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-12-19 22:07:39 +0000

    finally fetch jobs

--- app/javascript/controllers/jobs_index_controller.js
+++ app/javascript/controllers/jobs_index_controller.js
@@ -1,5 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
+const url = "https://jsearch.p.rapidapi.com/search";
+const loaded = false;
+const params = new URLSearchParams({
+  query: 'Fullstack developer in Germany',
+  page: '1',
+  num_pages: '20'
+});
+
+const headers = {
+  'X-RapidAPI-Key': '35cf0f26f2mshf30474c3c0fabe1p162696jsnce0701eed772',
+  'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+};
 // Connects to data-controller="jobs-index"
 export default class extends Controller {
   static targets = ['results']
@@ -8,16 +20,41 @@ export default class extends Controller {
   connect() {
     console.log("hello from the jobs controller")
     this.resultsTarget.innerHTML = ""
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-      .then(response => response.json())
-      .then((data) => {
-        data.results.forEach((result) => {
-          const jobTag = `<li class = "list-group-item border-0">
-            <p>${result.name}</p>
-          </li>`
+    // fetch("https://pokeapi.co/api/v2/pokemon/")
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     data.results.forEach((result) => {
+    //       const jobTag = `<li class = "list-group-item border-0">
+    //         <p>${result.name}</p>
+    //       </li>`
 
-          this.resultsTarget.insertAdjacentHTML("beforeend", jobTag);
+    //       this.resultsTarget.insertAdjacentHTML("beforeend", jobTag);
+    //     })
+    //   })
+
+    fetch(`${url}?${params}`, {
+      method: 'GET',
+      headers: headers
+    }).then(response => response.json())
+      .then(data => {
+        data.data.forEach((job) => {
+          console.log(job)
+          const jobCard = `<div class="card m-1" style="width: 18rem;">
+
+          <div class="card-body">
+            <h4 class="card-title">${job.employer_name}</h4>
+            <small style = "color:green">${job.job_title}</small> |
+            <small>${job.job_city}, ${job.job_country}</small>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>`
+
+        this.resultsTarget.insertAdjacentHTML("beforeend", jobCard);
         })
       })
+        .catch(error => {
+          console.log('Error:', error)
+        });
   }
 }
