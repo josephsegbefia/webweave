0b536608e1af308f4bcc7af82817c919bc866ba9
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-12-20 12:15:06 +0000

    Remove API-KEY

--- app/javascript/controllers/jobs_index_controller.js
+++ app/javascript/controllers/jobs_index_controller.js
@@ -9,24 +9,31 @@ const params = new URLSearchParams({
   num_pages: '20'
 });
 
-const headers = {
-  'X-RapidAPI-Key': '35cf0f26f2mshf30474c3c0fabe1p162696jsnce0701eed772',
-  'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-};
+// const headers = {
+//   'X-RapidAPI-Key': '35cf0f26f2mshf30474c3c0fabe1p162696jsnce0701eed772',
+//   'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+// };
 
 
 // Connects to data-controller="jobs-index"
 export default class extends Controller {
   static targets = ['results']
+  static values = {
+    apiKey: String
+  }
 
 
 
   connect() {
     console.log("hello from the jobs controller")
+    console.log(this.apiKeyValue);
     this.resultsTarget.innerHTML = ""
     fetch(`${url}?${params}`, {
       method: 'GET',
-      headers: headers
+      headers: {
+        'X-RapidAPI-Key': this.apiKeyValue,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      }
     }).then(response => response.json())
       .then(data => {
         console.log(data)
@@ -71,7 +78,7 @@ export default class extends Controller {
           <div class="card-body">
             <h4 class="card-title">${job.employer_name}</h4>
             <small style = "color:green">${job.job_title}</small> |
-            <small>${job.job_city}, ${job.job_country}</small>
+            <small>${job.job_city}
             <p class="card-text"></p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
